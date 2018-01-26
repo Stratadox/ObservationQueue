9103a3828728499df65787cd598050fb71d6e428
Stratadox <jesse@stratadox.com>
2018-01-26 16:34:30 +0100

    Alter Queue to use SplQueue over foreach loop

--- src/Queue.php
+++ src/Queue.php
@@ -4,27 +4,33 @@ declare(strict_types=1);
 
 namespace Stratadox\ObservationQueue;
 
+use SplQueue;
+
 class Queue implements QueuesObservations
 {
     private $observations;
 
     public function __construct()
     {
-        $this->observations = new Observations;
+        $this->observations = new SplQueue;
     }
 
     public function add($observer, string $method, ...$params) : void
     {
-        $this->observations = $this->observations->add(
+        $this->observations->enqueue(
             new Observation($observer, $method, ...$params)
         );
     }
 
     public function trigger() : void
     {
-        foreach ($this->observations as $observation) {
-            $this->observations = $this->observations->remove($observation);
-            $observation->trigger();
+        while (!$this->observations->isEmpty()) {
+            $this->process($this->observations->dequeue());
         }
+    }
+
+    private function process(Observation $observation) : void
+    {
+        $observation->trigger();
     }
 }
